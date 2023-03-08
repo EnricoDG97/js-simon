a09b3483a34e465df5ae51b45628fa183df3e375
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-03-08 10:46:25 +0100

    feat: exercise correction

--- script.js
+++ script.js
@@ -7,3 +7,95 @@ Bonus:
 Gestire l'inserimento dei numeri tramite 5 input diversi.
 */
 
+
+
+// 1- visualizziamo 5 numeri casuali
+const secretNumbersElement = document.getElementById("secretNumber");
+const outputTextElement = document.getElementById("outputText");
+
+// input utente
+// const number1Element = document.getElementById("number1");
+// const number2Element = document.getElementById("number2");
+// const number3Element = document.getElementById("number3");
+// const number4Element = document.getElementById("number4");
+// const number5Element = document.getElementById("number5");
+
+// array di campi di input
+const numberElements = [
+    document.getElementById("number1"),
+    document.getElementById("number2"),
+    document.getElementById("number3"),
+    document.getElementById("number4"),
+    document.getElementById("number5")
+];
+
+
+const checkButtonElement = document.getElementById("checkButton");
+
+// visualizzo in console i 5 numeri
+console.log(createRandomNumbers(5));
+// metto in variabile da poter usare
+const randomNumbersArray = createRandomNumbers(5);
+// stampo in pagina i numeri casuali
+secretNumbersElement.innerText = randomNumbersArray;
+
+
+//  2- faccio partire un timer di 10 seocndi
+//  dopo 10 secondi i numeri scompariranno
+setTimeout(function() {
+        disappearNumbers(secretNumbersElement)
+        document.getElementById("inputElementsContainer").style.display = "block";
+    }, 10000);
+
+// 3- alla presisone del pulsante
+checkButtonElement.addEventListener("click", function () {
+    let rightNumbersArray = checkNumbers(randomNumbersArray, numberElements)
+    // 4
+    endGame(rightNumbersArray, outputTextElement);
+});
+
+
+
+
+
+// FUNZIONI PER ESERCIZIO
+// 1
+function createRandomNumbers (quantity) {
+    randomNumbers = [];
+
+    for(let i=0; i < quantity; i++) {
+        randomNumbers.push(randomNumbersBetween(1, 10));
+    }
+
+    return randomNumbers;
+}
+
+// 2
+function disappearNumbers (numbersContainer) {
+    numbersContainer.innerText = "???";
+}
+
+// 3
+function checkNumbers (secretNumbers, userNumbers) {
+
+    const rightNumbers = [];
+
+    for(let i=0; i < userNumbers.length; i++) {
+        if(secretNumbers[i] == userNumbers[i]) {
+    
+            rightNumbers.push(secretNumbers[i]);
+            userNumbers[i].style.backgroundColor = "green";
+            console.log("trovato");
+        }
+
+        console.log(rightNumbers);
+    }
+
+    return rightNumbers;
+}
+
+// 4
+function endGame(rightNumbers, outputTextElement) {
+    outputTextElement.innerHTML = `Hai indivinato ${rightNumbers.length} numeri.<br>
+    I numeri che hai indovinato sono: ${rightNumbers}`;
+}
